5ab631728cc00e4cf257c2d1517c448eb69e4b8a
Felipe Augusto Bento Albuquerque <felipeaugustobento@hotmail.com>
2018-10-12 20:59:33 -0300

    Atualizando configuração minima para o django+heroku

--- meuBlog/settings.py
+++ meuBlog/settings.py
@@ -133,7 +133,5 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
--- meuBlog/wsgi.py
+++ meuBlog/wsgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meuBlog.settings')
 
-application = DjangoWhiteNoise(get_wsgi_application())
+application = Cling(get_wsgi_application())
