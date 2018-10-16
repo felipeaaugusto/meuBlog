b20793a457e42eb69facc759bb73f2160253d34e
Felipe Augusto Bento Albuquerque <felipeaugustobento@hotmail.com>
2018-10-15 22:36:38 -0300

    Buscando nome de usuário que adicionou post

--- content/views.py
+++ content/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from .models import Post
 
 # Create your views here.
 
 def home(request):
     posts = Post.objects.all()
+    for post in posts:
+        user = User.objects.get(id=post.added_by_id)
+        post.first_name = user.first_name
     return render(request, 'home.html',{'posts': posts})
