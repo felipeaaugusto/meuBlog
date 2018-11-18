2493a56788313cd8639a9600b8a91379bd790e10
Felipe <felipeaugustobento@hotmail.com>
2018-11-18 20:24:22 -0200

    Adicionando configuracao para usar arquivos estaticos no heroku

--- meuBlog/settings.py
+++ meuBlog/settings.py
@@ -14,9 +14,10 @@ import os
 import django_heroku
 from decouple import config
 from dj_database_url import parse as dburl
+from os.path import abspath, basename, dirname, join, normpath
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Important for Heroku
+BASE_DIR = dirname(dirname(abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -118,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'static' # Important for Heroku
 STATIC_URL = '/static/'    
 
 # Extra places for collectstatic to find static files.
